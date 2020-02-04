6aab42bbf8ae5e09174df31a74db702a3b407eb7
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-04 23:12:49 +0000

    adding middle function, and test cases using assertArraysEqual function

---
+++ middle.js
@@ -0,0 +1,34 @@
+//Test/Assertion Functions
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  let result = eqArrays(arr1, arr2);
+  return result ? 'The arrays are equal' : 'The arrays are not equal';
+};
+
+//Actual FUnction 
+const middle = function(arr) {
+  let output = []
+  let middle = Math.round((arr.length - 1) / 2);
+  if (arr.length > 2) {
+    if (arr.length % 2 !== 0){
+      output.push(arr[middle]);
+    } else if (arr.length % 2 === 0) {
+      output.push(arr[middle - 1], arr[middle]);
+    }
+  }
+  return output;
+}
+
+//Test Cases
+console.log(middle([1,2,3,4,5,6,9]))
+console.log(middle([1,6,4,8]));
+
+console.log(assertArraysEqual(middle([1, 6, 4, 3]), middle([3, 6, 3, 1])));
