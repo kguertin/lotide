ece1d4efadced8ebf75113c8793f8e9226c0f539
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-04 23:39:26 +0000

    adding minor refactor

--- middle.js
+++ middle.js
@@ -17,13 +17,17 @@ const assertArraysEqual = function(arr1, arr2) {
 const middle = function(arr) {
   let output = []
   let middle = Math.round((arr.length - 1) / 2);
-  if (arr.length > 2) {
-    if (arr.length % 2 !== 0){
-      output.push(arr[middle]);
-    } else if (arr.length % 2 === 0) {
-      output.push(arr[middle - 1], arr[middle]);
-    }
+
+  if (arr.length < 3){
+    return output;
   }
+
+  if (arr.length % 2 !== 0){
+    output.push(arr[middle]);
+  } else if (arr.length % 2 === 0) {
+    output.push(arr[middle - 1], arr[middle]);
+  }
+
   return output;
 }
 
@@ -31,4 +35,4 @@ const middle = function(arr) {
 console.log(middle([1,2,3,4,5,6,9]))
 console.log(middle([1,6,4,8]));
 
-console.log(assertArraysEqual(middle([1, 6, 4, 3]), middle([3, 6, 3, 1])));
+console.log(assertArraysEqual(middle([1, 2,  6, '4', 3, 8]), middle([3, 6, '4', 1])));
