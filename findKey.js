e2869691da664b459f5680f36d2a12222297da93
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-06 22:26:55 +0000

    adding findKey function

---
+++ findKey.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u{1F525} Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`\u{1F5D1} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback){
+  let objKeys = Object.keys(obj);
+  for (let num of objKeys){
+    if (callback(obj[num])){
+      return num;
+    }
+  }
+};
+
+
+console.log(
+findKey({
+  'Blue Hill': {stars: 1},
+  'Akaleri': {stars: 3},
+  'noma': {stars: 2},
+  'elBull': {stars: 3},
+  'Ora': {stars: 2},
+  'Akelarre': {stars: 3}
+}, x => x.stars === 2));
+
+const startingFive = {
+  PG: 'Kyle Lowry',
+  SG: 'Fred VanVleet',
+  SF: 'OG Anunoby',
+  PF: 'Pascal Siakam',
+  C: 'Marc Gasol'
+}
+
+console.log(findKey(startingFive, x => x === 'Fred VanVleet'))
+assertEqual(findKey(startingFive, x => x === 'Kyle Lowry'), 'PG');
