ef95fbefac3ccd87e337a7cfac9aca92119439ae
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-10 18:00:36 +0000

    commiting assertArraysEqual and test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,8 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
   let result = eqArrays(arr1, arr2);
   return result ? 'The arrays are equal' : 'The arrays are not equal';
 };
 
-console.log(assertArraysEqual([1,2,3],[1,2,3]));
-console.log(assertArraysEqual([1,2,3],[1,2,'3']));
+module.exports = assertArraysEqual;
