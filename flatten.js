018faa413d0f5e2c7bee14c5e46edeff22b01971
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-10 23:48:27 +0000

    published updates and mocha chai

--- flatten.js
+++ flatten.js
@@ -1,17 +1,3 @@
-//Test/Assertion Functions
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  let result = eqArrays(arr1, arr2);
-  return result ? 'The arrays are equal' : 'The arrays are not equal';
-};
 
 // Actual Function
 const flatten = function(arr){
