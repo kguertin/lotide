a5475cfe79d039a26ae0ec9bc689dd1e9a6798cc
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-10 19:13:55 +0000

    finished refactoring tail function

--- test/tailTest.js
+++ test/tailTest.js
@@ -7,9 +7,10 @@ describe('#Tail', () => {
   it('Should return an array of three items when passed an array of four items', () => {
     assert.deepEqual(tail([1,2,3,4]), [2,3,4]);
   })
-})
-
-// const [1,2,3,4]
-// const words = ['Toronto', 'Raptors', 'NBA', 'Champions'];
-// tail(words);
-// assertEqual(words.length, 4);
+  it('should return an empty arry if givena an array of lest than 2', () => {
+    assert.deepEqual(tail([1]), []);
+  })
+  it('should return an array of strings if passed an array of strings with more than one element', () => {
+    assert.deepEqual(tail(['toronto', 'raptors']), ['raptors']);
+  })
+});
