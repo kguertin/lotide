ce3593f61cd41ceef1794f7275a2f56ca8f0642a
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-10 19:08:12 +0000

    adding mocha chai to test file

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,15 @@
+const assert = require('chai').assert
+
 const tail = require('../tail');
 const assertEqual = require('../assertEqual');
 
-const words = ['Toronto', 'Raptors', 'NBA', 'Champions'];
-tail(words);
-assertEqual(words.length, 4);
+describe('#Tail', () => {
+  it('Should return an array of three items when passed an array of four items', () => {
+    assert.deepEqual(tail([1,2,3,4]), [2,3,4]);
+  })
+})
+
+// const [1,2,3,4]
+// const words = ['Toronto', 'Raptors', 'NBA', 'Champions'];
+// tail(words);
+// assertEqual(words.length, 4);
